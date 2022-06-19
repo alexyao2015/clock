fc9990c382d05e2591c59ee91dbb15f00f775330
Alex Yao <33379584+alexyao2015@users.noreply.github.com>
2022-06-19 05:20:38 +0000

    Add default view

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,20 +4,26 @@ import Navbar from "./components/Navbar.vue";
 
 <template>
   <v-app>
-    <Navbar />
     <v-main>
+      <Navbar />
       <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <style>
-:root {
+html {
+  overflow-y: auto;
+}
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.v-application__wrap {
+  min-height: unset !important;
 }
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -13,14 +13,3 @@ import "../lit/punch-app";
   <router-link to="/foo">Foo</router-link>
   <router-view></router-view> -->
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
