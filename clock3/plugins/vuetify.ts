276cf9fa2a34570e779523627a669268a49316ae
Alex Yao <33379584+alexyao2015@users.noreply.github.com>
2022-06-20 21:21:03 +0000

    Fix vuetify

--- clock3/plugins/vuetify.ts
+++ clock3/plugins/vuetify.ts
@@ -2,9 +2,13 @@ import { defineNuxtPlugin } from "#app";
 import { createVuetify } from "vuetify";
 import { aliases, mdi } from "vuetify/iconsets/mdi-svg";
 import "vuetify/styles";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
+    components,
+    directives,
     icons: {
       defaultSet: "mdi",
       aliases,
