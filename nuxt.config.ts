920e291e766ebd922d62f69e3377315ea0cc9a14
alexyao2015 <33379584+alexyao2015@users.noreply.github.com>
2022-06-21 04:22:09 +0000

    Add nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+import { defineNuxtConfig } from "nuxt";
+import vuetify from "vite-plugin-vuetify";
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  vite: {
+    // build: {
+    //   sourcemap: "inline",
+    // },
+    build: {
+      transpile: ['vuetify'],
+    },
+    ssr: {
+      noExternal: ["vuetify"],
+    },
+      server: {
+        // port: 3000,
+        hmr: {
+          // protocol: "wss",
+          clientPort: 3001,
+          port: 3001,
+          // host: "alexyao2015-clock-9wvvgqjj3xx4v-3001.githubpreview.dev",
+        },
+      },
+  },
+  modules: [
+    async (options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) =>
+        config.plugins.push(vuetify({ autoImport: true }))
+      );
+    },
+  ],
+});
